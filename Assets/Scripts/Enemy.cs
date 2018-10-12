9f60d94663546bb289ad23b2c9b65bd82d3c2f50
ncampoli <ncampoli21@gmail.com>
2018-10-12 18:26:32 -0400

    changes

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -2,11 +2,11 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour {
+public class Enemy : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		//YOLOI
+		
 	}
 	
 	// Update is called once per frame
